a349689a7a8a73d3b110ce78b8c45758a425be8e
sanuvarghese <sanuv18@gmail.com>
2025-05-29 08:04:07 +0530

    run also the html generation script

--- run_all.sh
+++ run_all.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+echo "[`date`] Starting run_all.sh"
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 cd /afs/cern.ch/work/s/savarghe/private/2024_Rate_test/CMSSW_15_0_3/src
 eval `scramv1 runtime -sh`
-
 # Go to working directory
 cd DQM
-
+echo "[`date`] Environment ready, starting Python"
 # Run scripts
 python3 unpack.py
-python3 compute_eff.py
+python3 compute_eff.py 
 python3 plot_all.py
 python3 plot_each_filter.py
+python3 website/generate_html_index.py
+echo "[`date`] Finished compute_eff and plots updated"
